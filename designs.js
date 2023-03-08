839ef20a3cbc34195d8057ce5240743374c77b91
leo shmukler <leonidishmukler@gmail.com>
2023-03-08 17:46:12 -0500

    ran beautify and added comments

--- designs.js
+++ designs.js
@@ -1,3 +1,4 @@
+// Set Variables for HTML elements
 const inputHeight = document.getElementById('inputHeight');
 const inputWidth = document.getElementById('inputWidth');
 const pixelCanvas = document.getElementById('pixelCanvas');
@@ -5,34 +6,37 @@ const colorPicker = document.getElementById('colorPicker');
 const sizePicker = document.getElementById('sizePicker');
 let selectedColor = colorPicker.value;
 
-
+// listen for color picker changes
 colorPicker.addEventListener('input', () => {
-  selectedColor = colorPicker.value;
+	selectedColor = colorPicker.value;
 });
-
+ 
+// listen for form submit - prevent default for submit event - call clear grid function- call create new grid function
 sizePicker.addEventListener('submit', (event) => {
-  event.preventDefault();
-  resetGrid();
-  makeGrid();  
+	event.preventDefault();
+	resetGrid();
+	makeGrid();
 });
 
-
+// create new grid
 function makeGrid() {
-  const rows = inputHeight.value;
-  const columns = inputWidth.value;
+	const rows = inputHeight.value;
+	const columns = inputWidth.value;
 
-  for (let r = 0; r < rows; r++) {
-    const row = pixelCanvas.insertRow(r);
-    for (let c = 0; c < columns; c++) {
-      const cell = row.insertCell(c);
-      cell.addEventListener('click', () => {
-        cell.style.backgroundColor = selectedColor;
-      })
-    }
-  }
+	for (let r = 0; r < rows; r++) {
+		const row = pixelCanvas.insertRow(r);
+		for (let c = 0; c < columns; c++) {
+			const cell = row.insertCell(c);
+			cell.addEventListener('click', () => {
+				cell.style.backgroundColor = selectedColor;
+			})
+		}
+	}
 }
 
+// clear grid
 function resetGrid() {
-while (pixelCanvas.firstChild) {
-  pixelCanvas.removeChild(pixelCanvas.firstChild);
-}}
+	while (pixelCanvas.firstChild) {
+		pixelCanvas.removeChild(pixelCanvas.firstChild);
+	}
+}
